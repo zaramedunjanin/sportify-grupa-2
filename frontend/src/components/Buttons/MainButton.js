47892e085447d78f50c4af8ad87ec6f474d59295
Ena Isen <enaisen@gmail.com>
2023-05-01 13:49:21 +0200

    Added link to signup and onClick to MainButton

--- frontend/src/components/Buttons/MainButton.js
+++ frontend/src/components/Buttons/MainButton.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './MainButton.scss';
+import "./MainButton.scss";
 
 const MainButton = (props) => {
-    return (
-        <a href={props.link}>
-            <button type={props.type} className="main-button background-orange">{props.text}</button>
-        </a>
-    )
-}
+  return (
+    <a href={props.link} onClick={props.onClick}>
+      <button type={props.type} className="main-button background-orange">
+        {props.text}
+      </button>
+    </a>
+  );
+};
 
-export default MainButton
+export default MainButton;
