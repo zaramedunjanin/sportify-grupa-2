26721820d091580868a55bfbb6ec74d244f39092
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2023-05-01 12:06:39 +0200

    added footer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,14 +4,16 @@ import Navbar from '../components/Navbar/Navbar';
 import Header from "../components/Header/Header";
 import Categories from "../components/Home/Categories";
 import About from "../components/Home/About";
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
     return (
         <>
-            <Navbar/>
-            <Header/>
-            <Categories/>
-            <About/>
+            <Navbar />
+            <Header />
+            <Categories />
+            <About />
+            <Footer />
         </>
     )
 }
