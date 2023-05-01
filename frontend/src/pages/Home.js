83f6cf2dd359585e76d22007e35c4916e971dc38
Zara Medunjanin <96110353+zaramedunjanin@users.noreply.github.com>
2023-05-01 12:25:27 +0200

    Revert "9-footer-content"

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,16 +4,14 @@ import Navbar from '../components/Navbar/Navbar';
 import Header from "../components/Header/Header";
 import Categories from "../components/Home/Categories";
 import About from "../components/Home/About";
-import Footer from '../components/Footer/Footer';
 
 const Home = () => {
     return (
         <>
-            <Navbar />
-            <Header />
-            <Categories />
-            <About />
-            <Footer />
+            <Navbar/>
+            <Header/>
+            <Categories/>
+            <About/>
         </>
     )
 }
