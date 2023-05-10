ac445209e13ee5018ba95cd42fcbd9101cf10c15
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2023-05-10 23:28:42 +0200

    css updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./components/pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FrequentlyAskedQuestions from './pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
+import FrequentlyAskedQuestions from './components/pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 import Signup from "./components/pages/Signup/SignupPage";
 import LoginPage from "./components/pages/Login/LoginPage";
 const App = () => {
