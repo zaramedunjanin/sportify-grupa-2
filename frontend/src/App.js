02ea91674dabd45aa10b51650ac06c07d053132a
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2023-05-13 12:13:29 +0200

    14 FAQ page (#26)
    * faq page added
    
    * nav added
    
    * css updated
    
    * removed body css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./components/pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FrequentlyAskedQuestions from './components/pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 import Signup from "./components/pages/Signup/SignupPage";
 import LoginPage from "./components/pages/Login/LoginPage";
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<FrequentlyAskedQuestions />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
