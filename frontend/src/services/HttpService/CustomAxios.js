fb1ab52bde330864232f63dc0411afec836beb8e
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2023-05-14 11:47:05 +0200

    formik, react query and axios service added

---
+++ frontend/src/services/HttpService/CustomAxios.js
@@ -0,0 +1,17 @@
+// all requests will go through this service
+// TODO: add get, post, put and delete methodss
+// TODO: make api path an ENV variables
+// TODO: handle token attachment and expiration here
+import axios from "axios";
+
+const baseURL = "http://127.0.0.1:8000";
+const token = localStorage.getItem('token');
+
+const instance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+});
+
+export default instance;
--- frontend/src/services/HttpService/HttpService.js
+++
@@ -1,4 +0,0 @@
-// all requests will go through this service
-// TODO: add get, post, put and delete methodss
-// TODO: make api path an ENV variables
-// TODO: handle token attachment and expiration here
