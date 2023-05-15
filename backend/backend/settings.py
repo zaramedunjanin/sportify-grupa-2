fafca37b4414eaceeb9f26beb2cb13a53bbf82ee
Zara Medunjanin <zara.medunjanin@gmail.com>
2023-05-15 13:33:46 +0200

    Added connection to PostgreSQL in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,6 +89,17 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mydatabase',
+            'USER': 'mydatabaseuser',
+            'PASSWORD': 'mypassword',
+            'HOST': 'mydatabasehost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
