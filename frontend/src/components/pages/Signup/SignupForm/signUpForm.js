c34fd244243f1829c073dd3bcc4dbc0b3335d636
babacicemir <emirbabacix7@gmail.com>
2023-05-16 18:31:38 +0200

    updated login and signup

--- frontend/src/components/pages/Signup/SignupForm/signUpForm.js
+++ frontend/src/components/pages/Signup/SignupForm/signUpForm.js
@@ -8,76 +8,87 @@ import MainButton from "../../../atoms/Buttons/MainButton/MainButton";
 
 const SignUpForm = () => {
   return (
-    <Container className="inputContainer">
-      <Row className="inputBackground">
-        <Col xs={6} className="pr-2">
-          <input type="text" id="fname" name="fname" placeholder="First Name" />
-        </Col>
-        <Col xs={6} className="pl-2">
-          <input
-            className="closer"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Last Name"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="longInput"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="City"
-          />
-        </Col>
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="longInput"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Phone Number"
-          />
-        </Col>
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="longInput"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Username"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="longInput"
-            type="email"
-            id="fname"
-            name="fname"
-            placeholder="Email"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="longInput"
-            type="password"
-            id="fname"
-            name="fname"
-            placeholder="Password"
-          />
-        </Col>
-
-        <Row style={{ paddingRight: "0" }}>
-          <Col className="centerContainer mt-4">
-            <MainButton text={"Sign Up"} />
+      <Container className="inputContainer">
+        <Row className="inputBackground">
+          <Col xs={6} className="pr-2">
+            <input type="text" className="input" id="fname" name="fname" placeholder="First Name" />
           </Col>
+          <Col xs={6} className="pl-2">
+            <input
+                className="input"
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="Last Name"
+            />
+          </Col>
+
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                className="input"
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="City"
+            />
+          </Col>
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                className="input"
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="Phone Number"
+            />
+          </Col>
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                className="input"
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="Username"
+            />
+          </Col>
+
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                className="input"
+                type="email"
+                id="fname"
+                name="fname"
+                placeholder="Email"
+            />
+          </Col>
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                className="input"
+                type="password"
+                id="fname"
+                name="fname"
+                placeholder="Password"
+            />
+          </Col>
+
+          <Col xs={12} className="mt-3 pt-1">
+            <select className="input"   id="fname" name="fname" placeholder="Role">
+              <option  value=""  disabled selected hidden>Choose a role</option>
+              <option value="1">Admin</option>
+              <option value="2">Company</option>
+              <option value="3">User</option>
+            </select>
+          </Col>
+
+          <Row style={{ paddingRight: "0" }}>
+            <Col className="centerContainer mt-4">
+              <MainButton className= "form-button" text={"Sign Up"} />
+            </Col>
+          </Row>
         </Row>
-      </Row>
-    </Container>
+      </Container>
   );
 };
 
--- frontend/src/components/pages/Signup/SignupRightSide/signupRightSide.js
+++ frontend/src/components/pages/Signup/SignupRightSide/signupRightSide.js
@@ -10,25 +10,25 @@ import MainButton from "../../../atoms/Buttons/MainButton/MainButton";
 import { useNavigate } from "react-router-dom";
 
 const SignupRightSide = () => {
-  const navigate = useNavigate();
-  return (
-    <Container fluid className="background-green height">
-      <Row>
-        <Col xs={6}>
-          <Logo />
-        </Col>
-      </Row>
-      <Row className="p-0">
-        <Col>
-          <h3>Already have an account?</h3>
-          <p>Log in and catch up with the latest events.</p>
-          <div className="centerContainer">
-            <MainButton text={"Log In"} onClick={() => navigate("/login")} />
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+    const navigate = useNavigate();
+    return (
+        <Container fluid className="background-green height">
+            <Row>
+                <Col xs={6}>
+                    <Logo />
+                </Col>
+            </Row>
+            <Row className="p-0">
+                <Col>
+                    <h3 className="h3_center">Already have an account?</h3>
+                    <p>Log in and catch up with the latest events.</p>
+                    <div className="centerContainer">
+                        <MainButton text={"Log In"} onClick={() => navigate("/login")} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default SignupRightSide;
