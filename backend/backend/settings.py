97bf43dd7551095fda511d1c08d42f90a0b9ef17
Zara Medunjanin <zara.medunjanin@gmail.com>
2023-05-16 22:35:13 +0200

    README.md

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,14 +91,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sportifydb',
         'USER': 'sportify',
         'PASSWORD': 'sportify',
