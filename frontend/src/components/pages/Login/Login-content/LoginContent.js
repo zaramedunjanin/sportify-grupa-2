725524bfacb7a5567454853a953602fde478c4bd
EnaIsen <enaisen@gmail.com>
2023-05-17 20:44:38 +0200

    Edited Login and Signup

--- frontend/src/components/pages/Login/Login-content/LoginContent.js
+++ frontend/src/components/pages/Login/Login-content/LoginContent.js
@@ -11,26 +11,26 @@ import { useNavigate } from "react-router-dom";
 const LoginContent = () => {
   const navigate = useNavigate();
   return (
-      <>
-        <Container fluid className={"container"}>
-          <Row>
-            <Col sm={5}>
-              <Navbar />
-              <div className="bg-lightgreen left-side">
-                <h3 className="login-h">Don't have an account?</h3>
-                <p>Join with the peeeeepz</p>
-                <Button  text={"Sign up"} onClick={() => navigate("/signup")} />
-              </div>
-            </Col>
-            <Col className="right-side d-flex justify-content-center " sm={7}>
-              <div>
-                <h2 className="display-8">Welcome back!</h2>
-                <LoginForm />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </>
+    <>
+      <Container fluid className={"container_1"}>
+        <Row>
+          <Col sm={5}>
+            <Navbar />
+            <div className="bg-lightgreen left-side">
+              <h3 className="login-h">Don't have an account?</h3>
+              <p>Join with the peeeeepz</p>
+              <Button text={"Sign up"} onClick={() => navigate("/signup")} />
+            </div>
+          </Col>
+          <Col className="right-side d-flex justify-content-center " sm={7}>
+            <div>
+              <h2 className="display-8">Welcome back!</h2>
+              <LoginForm />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- frontend/src/components/pages/Signup/SignupForm/signUpForm.js
+++ frontend/src/components/pages/Signup/SignupForm/signUpForm.js
@@ -8,87 +8,81 @@ import MainButton from "../../../atoms/Buttons/MainButton/MainButton";
 
 const SignUpForm = () => {
   return (
-      <Container className="inputContainer">
-        <Row className="inputBackground">
-          <Col xs={6} className="pr-2">
-            <input type="text" className="input" id="fname" name="fname" placeholder="First Name" />
-          </Col>
-          <Col xs={6} className="pl-2">
-            <input
-                className="input"
-                type="text"
-                id="fname"
-                name="fname"
-                placeholder="Last Name"
-            />
-          </Col>
+    <Container className="inputContainer">
+      <Row className="inputBackground">
+        <Col xs={6} className="pr-2">
+          <input
+            type="text"
+            className="input"
+            id="fname"
+            name="fname"
+            placeholder="First Name"
+          />
+        </Col>
+        <Col xs={6} className="pl-2">
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="Last Name"
+          />
+        </Col>
 
+        <Col xs={12} className="mt-3 pt-1">
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="City"
+          />
+        </Col>
+        <Col xs={12} className="mt-3 pt-1">
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="Phone Number"
+          />
+        </Col>
+        <Col xs={12} className="mt-3 pt-1">
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="Username"
+          />
+        </Col>
 
-          <Col xs={12} className="mt-3 pt-1">
-            <input
-                className="input"
-                type="text"
-                id="fname"
-                name="fname"
-                placeholder="City"
-            />
-          </Col>
-          <Col xs={12} className="mt-3 pt-1">
-            <input
-                className="input"
-                type="text"
-                id="fname"
-                name="fname"
-                placeholder="Phone Number"
-            />
-          </Col>
-          <Col xs={12} className="mt-3 pt-1">
-            <input
-                className="input"
-                type="text"
-                id="fname"
-                name="fname"
-                placeholder="Username"
-            />
-          </Col>
+        <Col xs={12} className="mt-3 pt-1">
+          <input
+            className="input"
+            type="email"
+            id="fname"
+            name="fname"
+            placeholder="Email"
+          />
+        </Col>
 
-
-          <Col xs={12} className="mt-3 pt-1">
-            <input
-                className="input"
-                type="email"
-                id="fname"
-                name="fname"
-                placeholder="Email"
-            />
+        <Col xs={12} className="mt-3 pt-1">
+          <input
+            className="input"
+            type="password"
+            id="fname"
+            name="fname"
+            placeholder="Password"
+          />
+        </Col>
+        <Row style={{ paddingRight: "0" }}>
+          <Col className="centerContainer mt-4">
+            <MainButton className="form-button" text={"Sign Up"} />
           </Col>
-
-          <Col xs={12} className="mt-3 pt-1">
-            <input
-                className="input"
-                type="password"
-                id="fname"
-                name="fname"
-                placeholder="Password"
-            />
-          </Col>
-
-          <Col xs={12} className="mt-3 pt-1">
-            <select className="input"   id="fname" name="fname" placeholder="Role">
-              <option  value=""  disabled selected hidden>Choose a role</option>
-              <option value="1">Admin</option>
-              <option value="2">Company</option>
-              <option value="3">User</option>
-            </select>
-          </Col>
-
-          <Row style={{ paddingRight: "0" }}>
-            <Col className="centerContainer mt-4">
-              <MainButton className= "form-button" text={"Sign Up"} />
-            </Col>
-          </Row>
         </Row>
-      </Container>
+      </Row>
+    </Container>
   );
 };
 
