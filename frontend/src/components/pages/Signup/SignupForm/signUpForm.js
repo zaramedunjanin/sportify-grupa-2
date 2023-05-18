b30c6a5acdad37ec16eb1f374fe3c1260f3752bb
babacicemir <emirbabacix7@gmail.com>
2023-05-19 00:25:19 +0200

    added forms validation

--- frontend/src/components/pages/Signup/SignupForm/signUpForm.js
+++ frontend/src/components/pages/Signup/SignupForm/signUpForm.js
@@ -5,84 +5,133 @@ import Col from "react-bootstrap/esm/Col";
 
 import "../SignupForm/signUpForm.css";
 import MainButton from "../../../atoms/Buttons/MainButton/MainButton";
-
+import useSignUp from "../../../../hooks/useSignUp";
 const SignUpForm = () => {
+
+  const {
+    handleSubmit,
+    isDisabled,
+
+    firstName,
+    setFirstName,
+    firstNameError,
+
+    lastName,
+    setLastName,
+    lastNameError,
+
+    city,
+    setCity,
+    cityError,
+
+    phoneNumber,
+    setPhoneNumber,
+    phoneNumberError,
+
+    username,
+    setUsername,
+    usernameError,
+
+    password,
+    setPassword,
+    passwordError,
+
+    email,
+    setEmail,
+    emailError,
+  } = useSignUp()
+
+
+
   return (
-    <Container className="inputContainer">
-      <Row className="inputBackground">
-        <Col xs={6} className="pr-2">
-          <input
-            type="text"
-            className="input"
-            id="fname"
-            name="fname"
-            placeholder="First Name"
-          />
-        </Col>
-        <Col xs={6} className="pl-2">
-          <input
-            className="input"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Last Name"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="input"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="City"
-          />
-        </Col>
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="input"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Phone Number"
-          />
-        </Col>
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="input"
-            type="text"
-            id="fname"
-            name="fname"
-            placeholder="Username"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="input"
-            type="email"
-            id="fname"
-            name="fname"
-            placeholder="Email"
-          />
-        </Col>
-
-        <Col xs={12} className="mt-3 pt-1">
-          <input
-            className="input"
-            type="password"
-            id="fname"
-            name="fname"
-            placeholder="Password"
-          />
-        </Col>
-        <Row style={{ paddingRight: "0" }}>
-          <Col className="centerContainer mt-4">
-            <MainButton className="form-button" text={"Sign Up"} />
+      <Container className="inputContainer">
+        <Row className="inputBackground">
+          <Col xs={6} className="pr-2">
+            <input
+                type="text"
+                className={`input ${firstNameError ? "is-invalid" : ""}`}
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            {firstNameError && (<div className="text-danger small">{firstNameError}</div>)}
           </Col>
+          <Col xs={6} className="pl-2">
+            <input
+                type="text"
+                className={`input ${lastNameError ? "is-invalid" : ""}`}
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            {lastNameError && (<div className="text-danger small">{lastNameError}</div>)}
+          </Col>
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                type="text"
+                className={`input ${cityError ? "is-invalid" : ""}`}
+                placeholder="City"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+            />
+            {cityError && <div className="text-danger small">{cityError}</div>}
+          </Col>
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                type="text"
+                className={`input ${phoneNumberError ? "is-invalid" : ""}`}
+                placeholder="Phone Number"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            {phoneNumberError && <div className="text-danger small">{phoneNumberError}</div>}
+          </Col>
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                type="text"
+                className={`input ${usernameError ? "is-invalid" : ""}`}
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameError && <div className="text-danger small">{usernameError}</div>}
+          </Col>
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                type="email"
+                className={`input ${emailError ? "is-invalid" : ""}`}
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailError && <div className="text-danger small">{emailError}</div>}
+          </Col>
+
+          <Col xs={12} className="mt-3 pt-1">
+            <input
+                type="password"
+                className={`input ${passwordError ? "is-invalid" : ""}`}
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordError && <div className="text-danger small">{passwordError}</div>}
+          </Col>
+
+          <Row style={{ paddingRight: "0" }}>
+            <Col className="centerContainer mt-4">
+              <MainButton
+                  className="form-button"
+                  text={"Sign Up"}
+                  onClick={handleSubmit}
+                  disabled={isDisabled}
+              />
+            </Col>
+          </Row>
         </Row>
-      </Row>
-    </Container>
+      </Container>
   );
 };
 
