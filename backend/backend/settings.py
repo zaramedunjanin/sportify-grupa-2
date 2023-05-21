704e2dbefdb97e7aafbb66e1aed078084ec93051
EnaIsen <enaisen@gmail.com>
2023-05-21 20:03:30 +0200

    General Layout

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sportifydb',
-        'USER': 'sportify',
-        'PASSWORD': 'sportify',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
