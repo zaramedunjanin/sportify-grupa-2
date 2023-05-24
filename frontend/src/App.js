bf7cd93c69861e273418ce8c1f59fe4e44189194
EnaIsen <enaisen@gmail.com>
2023-05-24 23:54:29 +0200

    Added frontend User Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Home from "./components/pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FrequentlyAskedQuestions from './components/pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
+import FrequentlyAskedQuestions from "./components/pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions";
 import Signup from "./components/pages/Signup/SignupPage";
 import LoginPage from "./components/pages/Login/LoginPage";
+import UserDashboardPage from "./components/pages/UserDashboard/UserDashboardPage";
+import UserData from "./components/pages/UserDashboard/UserData/UserData";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +15,8 @@ const App = () => {
         <Route path="/faq" element={<FrequentlyAskedQuestions />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/userdashboard" element={<UserDashboardPage />} />
+        <Route path="/userdata" element={<UserData />} />
       </Routes>
     </BrowserRouter>
   );
