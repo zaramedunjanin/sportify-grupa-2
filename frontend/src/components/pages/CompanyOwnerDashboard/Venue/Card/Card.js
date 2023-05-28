29a416b8d95a74a16fb6ebef39c23f7780fff41f
almir.handabaka <almir.handabaka@gmail.com>
2023-05-28 14:52:55 +0200

    option dropdown

---
+++ frontend/src/components/pages/CompanyOwnerDashboard/Venue/Card/Card.js
@@ -0,0 +1,120 @@
+import myImage from "./placeholder.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
+import { faFutbol } from "@fortawesome/free-solid-svg-icons";
+import { faBasketball } from "@fortawesome/free-solid-svg-icons";
+import { faVolleyball } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+
+import "./Card.css";
+
+const Card = ({ venue, ...rest }) => {
+  const formatTime = (timeString) => {
+    const [hours, minutes] = timeString.split(":");
+    const parsedTime = `${hours}:${minutes}`;
+
+    return parsedTime;
+  };
+
+  return (
+    <>
+      <div
+        className="card min-width-card mb-5"
+        style={{ width: "17rem", fontSize: "12px" }}
+      >
+        <img src={myImage} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              {/* <div className="card-options">
+                <FontAwesomeIcon icon={faEllipsis} className="me-1 ms-1" />
+              </div> */}
+
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle card-options"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faEllipsis} className="me-1 ms-1" />
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start shadow p-2">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="row fw-bolder fs-6">
+                <p
+                  className="card-title"
+                  style={{ marginLeft: "0px !important" }}
+                >
+                  {venue.venue_name}
+                  <FontAwesomeIcon icon={faFutbol} className="me-1 ms-1" />
+                  <FontAwesomeIcon icon={faBasketball} className="me-1" />
+                  <FontAwesomeIcon icon={faVolleyball} className="me-1" />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="row fw-semibold" style={{ fontSize: "16px" }}>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faLocationDot} className="me-1" />{" "}
+                  {venue.address}{" "}
+                </p>
+              </div>
+              <div className="row fw-semibold" style={{ fontSize: "16px" }}>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faStar} className="me-1" />
+                  4.84{" "}
+                </p>
+              </div>
+              <div className="row fw-semibold" style={{ fontSize: "16px" }}>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faClock} className="me-1" />
+                  {formatTime(venue.opening_time)}h -{" "}
+                  {formatTime(venue.closing_time)}h{" "}
+                </p>
+              </div>
+              <div className="row fw-semibold" style={{ fontSize: "16px" }}>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon
+                    icon={faTag}
+                    className="me-1"
+                    rotation={90}
+                  />
+                  {venue.price_per_hour} KM{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- frontend/src/components/pages/CompanyOwnerDashboard/Venue/OwnerVenue.js
+++ frontend/src/components/pages/CompanyOwnerDashboard/Venue/OwnerVenue.js
@@ -1,5 +1,61 @@
+import Card from "./Card/Card";
+
 const OwnerVenue = () => {
-  return <>Venue owner page</>;
+  const result = [
+    {
+      venue_name: "venue 1",
+      address: "address 1",
+      opening_time: "08:00",
+      closing_time: "21:00",
+      price_per_hour: "25",
+    },
+    {
+      venue_name: "venue 1",
+      address: "address 1",
+      opening_time: "08:00",
+      closing_time: "21:00",
+      price_per_hour: "25",
+    },
+    {
+      venue_name: "venue 1",
+      address: "address 1",
+      opening_time: "08:00",
+      closing_time: "21:00",
+      price_per_hour: "25",
+    },
+    {
+      venue_name: "venue 1",
+      address: "address 1",
+      opening_time: "08:00",
+      closing_time: "21:00",
+      price_per_hour: "25",
+    },
+    {
+      venue_name: "venue 1",
+      address: "address 1",
+      opening_time: "08:00",
+      closing_time: "21:00",
+      price_per_hour: "25",
+    },
+  ];
+
+  return (
+    <>
+      <div className="container mt-4 d-flex justify-content-center">
+        <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1">
+          {result &&
+            result.length !== 0 &&
+            result.map((venue) => {
+              return (
+                <div className="col d-flex justify-content-center">
+                  <Card venue={venue} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OwnerVenue;
