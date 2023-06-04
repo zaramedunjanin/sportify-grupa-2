6a98696cb380b3cbc0a9ecb661050b491bd7acbb
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2023-06-04 11:24:05 +0200

    calendar example

--- frontend/src/components/pages/CompanyOwnerDashboard/Venue/OwnerVenue.js
+++ frontend/src/components/pages/CompanyOwnerDashboard/Venue/OwnerVenue.js
@@ -4,6 +4,9 @@ import DeleteModal from "./Modal/DeleteModal";
 import VenueModal from "./Modal/VenueModal";
 import { useState } from "react";
 
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
 const OwnerVenue = () => {
   const result = [
     {
@@ -50,6 +53,22 @@ const OwnerVenue = () => {
 
   const [deleteId, setDeleteId] = useState(0);
   const [editId, setEditId] = useState(0);
+
+  return (<div style={{ width: "800px" }
+  }>
+    <FullCalendar
+      height={650}
+      plugins={[timeGridPlugin]}
+      initialView="timeGridWeek"
+      weekends={true}
+      events={[
+        { title: 'event 1', date: '2023-06-01T10:30:00' },
+        { title: 'event 2', date: '2023-06-02' }
+      ]}
+
+    />
+  </div >
+  )
 
   return (
     <>
