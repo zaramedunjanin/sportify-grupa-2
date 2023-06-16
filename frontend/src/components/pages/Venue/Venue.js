3c29ec3816ed50b6a64484d57f4c999e66ce3d6e
EnaIsen <enaisen@gmail.com>
2023-06-16 16:47:04 +0200

    Added bubble for questions

--- frontend/src/components/pages/Venue/Venue.js
+++ frontend/src/components/pages/Venue/Venue.js
@@ -7,7 +7,9 @@ import styles from "./Venue.module.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import { Image } from "react-bootstrap";
+import Button from "../../atoms/Buttons/MainButton/MainButton";
 
 import slika from "../../../assets/images/pitch.jpg";
 import MainButton from "../../atoms/Buttons/MainButton/MainButton";
@@ -16,112 +18,143 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import SportCategoryButton from "../../atoms/Buttons/SportCategoryButton/SportCategoryButton";
 import ScheduleNowModal from "./ScheduleNowModal";
+import CustomButton from "../../atoms/Buttons/CustomButton";
 
 const Venue = () => {
-    let { id } = useParams();
-    const url = `http://127.0.0.1:8000/venue/${id}`;
+  let { id } = useParams();
+  const url = `http://127.0.0.1:8000/venue/${id}`;
 
-    const [venue, setVenue] = useState({});
-    const [sports, setSports] = useState([]);
-    const [rating, setRating] = useState("");
+  const [venue, setVenue] = useState({});
+  const [sports, setSports] = useState([]);
+  const [rating, setRating] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    async function fetchVenue() {
-        const response = await axios.get(`${url}`);
+  async function fetchVenue() {
+    const response = await axios.get(`${url}`);
 
-        if (response.data[0] === undefined) {
-            navigate(-1);
-        } else {
-            response.data[0].opening_time = response.data[0].opening_time.substring(
-                0,
-                5
-            );
-            response.data[0].closing_time = response.data[0].closing_time.substring(
-                0,
-                5
-            );
-            setVenue(response.data[0]);
-            setSports(response.data[0].sport);
-        }
-
-        const r = await axios.get(`${url}/getrating`);
-        if (r.data === 0.0 || typeof r.data !== "number") setRating("--");
-        else setRating(`${r.data}`);
+    if (response.data[0] === undefined) {
+      navigate(-1);
+    } else {
+      response.data[0].opening_time = response.data[0].opening_time.substring(
+        0,
+        5
+      );
+      response.data[0].closing_time = response.data[0].closing_time.substring(
+        0,
+        5
+      );
+      setVenue(response.data[0]);
+      setSports(response.data[0].sport);
     }
 
-    useEffect(() => {
-        fetchVenue();
-    }, []);
+    const r = await axios.get(`${url}/getrating`);
+    if (r.data === 0.0 || typeof r.data !== "number") setRating("--");
+    else setRating(`${r.data}`);
+  }
 
-    return (
-        <>
-            <Navbar />
-            <div className={styles.body}>
-                <Container>
-                    <Row>
-                        <Col className={`${styles.card} my-2`}>
-                            <Image className={styles.cover} fluid src={slika} />
-                            <Row className={"justify-content-between align-items-center"}>
-                                <Col xs={7} className={"align-items-center my-1"}>
-                                    <h2 className={`${styles.title} ms-2`}>{venue.venue_name}</h2>
-                                </Col>
-                                <Col className={"text-center"}>
-                                    <MainButton text={"Schedule Now"} onClick={handleShow} />
-                                </Col>
-                            </Row>
+  useEffect(() => {
+    fetchVenue();
+  }, []);
 
-                            {sports.map((sport) => {
-                                return <SportCategoryButton sport_name={sport} />;
-                            })}
+  return (
+    <>
+      <Navbar />
+      <div className={styles.body}>
+        <Container>
+          <Row>
+            <Col className={`${styles.card} my-2`}>
+              <Image className={styles.cover} fluid src={slika} />
+              <Row className={"justify-content-between align-items-center"}>
+                <Col xs={7} className={"align-items-center my-1"}>
+                  <h2 className={`${styles.title} ms-2`}>{venue.venue_name}</h2>
+                </Col>
+                <Col className={"text-center"}>
+                  <MainButton text={"Schedule Now"} onClick={handleShow} />
+                </Col>
+              </Row>
 
-                            <Row>
-                                <h5>
-                                    <span className="material-symbols-outlined ms-2">grade</span>{" "}
-                                    {rating}
-                                </h5>
-                                <h5>
+              {sports.map((sport) => {
+                return <SportCategoryButton sport_name={sport} />;
+              })}
+
+              <Row>
+                <h5>
+                  <span className="material-symbols-outlined ms-2">grade</span>{" "}
+                  {rating}
+                </h5>
+                <h5>
                   <span className="material-symbols-outlined ms-2">
                     location_on
                   </span>{" "}
-                                    {venue.address}
-                                </h5>
-                                <h5>
+                  {venue.address}
+                </h5>
+                <h5>
                   <span className="material-symbols-outlined ms-2">
                     schedule
                   </span>{" "}
-                                    {venue.opening_time} - {venue.closing_time}
-                                </h5>
-                                <h5>
+                  {venue.opening_time} - {venue.closing_time}
+                </h5>
+                <h5>
                   <span className="material-symbols-outlined ms-2">
                     payments
                   </span>{" "}
-                                    {venue.price_per_hour} KM/h
-                                </h5>
-                            </Row>
-                            <Row className={"mt-1"}>
-                                <h4>Description</h4>
-                                <p>{venue.description}</p>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>{/* faq */}</Row>
-                </Container>
-            </div>
-            <Footer />
-            <ScheduleNowModal
-                show={show}
-                handleClose={handleClose}
-                sports={sports}
-                url={url}
-            />
-        </>
-    );
+                  {venue.price_per_hour} KM/h
+                </h5>
+              </Row>
+              <Row className={"mt-1"}>
+                <h4>Description</h4>
+                <p>{venue.description}</p>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12}>
+              <Row className={`${styles.card_1} justify-content-center`}>
+                <Col>
+                  <p className={styles.heading_1}>Ask a question</p>
+                  <Form>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlTextarea1"
+                    >
+                      <Form.Control
+                        as="textarea"
+                        rows={3}
+                        id={`${styles.custom} ${styles.full_width}`}
+                        style={{ resize: "none", height: "auto" }}
+                      />
+                    </Form.Group>
+                  </Form>
+                  <div className={styles.button_wrapper}>
+                    <Button
+                      type="submit"
+                      text="Send"
+                      className={`${styles.ask_button} ${styles.smaller_button}`}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <Footer />
+      <ScheduleNowModal
+        show={show}
+        handleClose={handleClose}
+        sports={sports}
+        url={url}
+      />
+    </>
+  );
 };
 
 export default Venue;
