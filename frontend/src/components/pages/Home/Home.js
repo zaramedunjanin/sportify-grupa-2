1e6de4a48a1f3837143ba2617d919a5228d8c077
EnaIsen <enaisen@gmail.com>
2023-06-18 00:33:51 +0200

    Added and fixed for translate modal

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 import Navbar from "../../organisms/Navbar/Navbar";
 import Header from "../../organisms/Header/Header";
 import Categories from "./Categories/Categories";
-import About from "../About/About";
 import Footer from "../../organisms/Footer/Footer";
 
 const Home = () => {
