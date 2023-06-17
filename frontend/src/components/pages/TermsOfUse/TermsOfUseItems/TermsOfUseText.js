37a232237bc23865421af9bb08abf16d6fd5b37c
EnaIsen <enaisen@gmail.com>
2023-06-17 18:37:36 +0200

    Edited text for terms of use.

--- frontend/src/components/pages/TermsOfUse/TermsOfUseItems/TermsOfUseText.js
+++ frontend/src/components/pages/TermsOfUse/TermsOfUseItems/TermsOfUseText.js
@@ -16,9 +16,38 @@ const TermsOfUseText = () => {
 
   return (
     <Container fluid>
-      <p style={textStyle_1}>{t("terms_of_use_text_2")}</p>
       <p style={textStyle_1}>{t("terms_of_use_text")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title")}</p>
       <p style={textStyle_1}>{t("terms_of_use_text_1")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_1")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_2")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_3")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_2")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_4")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_5")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_3")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_6")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_4")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_7")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_8")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_5")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_9")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_6")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_10")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_7")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_11")}</p>
+      <br />
+      <p style={textStyle_1}>{t("terms_of_use_title_8")}</p>
+      <p style={textStyle_1}>{t("terms_of_use_text_12")}</p>
+      <br />
     </Container>
   );
 };
