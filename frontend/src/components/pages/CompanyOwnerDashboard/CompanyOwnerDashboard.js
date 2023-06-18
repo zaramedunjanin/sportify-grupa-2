756364a8545baa92fd91614fa4c7c3826ed7d374
almir.handabaka <almir.handabaka@gmail.com>
2023-06-18 20:44:57 +0200

    useEffectTitle added

--- frontend/src/components/pages/CompanyOwnerDashboard/CompanyOwnerDashboard.js
+++ frontend/src/components/pages/CompanyOwnerDashboard/CompanyOwnerDashboard.js
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../organisms/Navbar/Navbar";
+import useEffectTitle from "../../../hooks/useEffectTitle";
 
 const CompanyOwnerDashboard = () => {
+  useEffectTitle(`Company | Sportify`);
   return (
     <>
       <Navbar variant="company"></Navbar>
